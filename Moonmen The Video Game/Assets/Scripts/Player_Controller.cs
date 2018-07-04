2abc44b9bd4b628fea830689685ee6d946ded498
Christpher Aucoin <criscross03@gmail.com>
2018-07-04 17:36:18 -0500

    Improved Jump
    Added a jump varying on how long the player holds the button and also makes jumping feel less "floaty"

--- Moonmen The Video Game/Assets/Scripts/Player_Controller.cs
+++ Moonmen The Video Game/Assets/Scripts/Player_Controller.cs
@@ -1,54 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player_Controller : MonoBehaviour {
+public class Player_Controller : MonoBehaviour
+{
 
-    public int speed;
-    public float jump;
+    public int maxSpeed;
+    public float jumpVelocity;
+    public float fallMultiplyer;
+    public float lowJumpMultiplyer;
 
     //Movement vars
     Vector3 direction;
 
     //Used for time to jump
     double time1;
-	bool canJump = true;
+    bool canJump = true;
 
     //Declares Object Components
     Transform trans;
     Rigidbody2D rigidbody2D;
 
-	//Called when player is touching something
-	void OnCollisionEnter2D()
-	{
-		canJump = true;
-	}
+    //Called when player is touching something
+    void OnCollisionEnter2D()
+    {
+        canJump = true;
+    }
 
-	//Called when player not touching something
-	void OnCollisionExit2D()
-	{
-		canJump = false;
-	}
+    //Called when player not touching something
+    void OnCollisionExit2D()
+    {
+        canJump = false;
+    }
 
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         trans = gameObject.GetComponent<Transform>();
         rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-	}
+    }
 
     // Update is called once per frame
     void Update()
     {
-		
+
     }
 
-	void FixedUpdate(){
-		
-		//Updates movement every frame
-		movement ();
-	}
+    void FixedUpdate()
+    {
+
+        //Updates movement every frame
+        movement();
+    }
 
 
     //Function that deals with all the player movement
@@ -78,18 +81,35 @@ public class Player_Controller : MonoBehaviour {
 
 
         //Gets final speed that the "moonman" will go at
-        float xVec = xInput * (float)(speed * .01);
+        float xVec = xInput * (float)(maxSpeed * .01);
 
 
         //Moves the "moonman" horizontally
         trans.Translate(new Vector3(xVec, 0));
 
-        
+        /*
         //Decides whether the "moonman" should jump or not
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && canJump)
         {
             rigidbody2D.AddForce(new Vector2(0, jump * 100));
             time1 = Time.time;
+        }
+        */
+        Debug.Log(rigidbody2D.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody2D.velocity = Vector2.up * jumpVelocity;
+        }
+
+
+        if (rigidbody2D.velocity.y < 0)
+        {
+            rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplyer - 1) * Time.deltaTime;
+        }
+
+        if (rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * lowJumpMultiplyer * Time.deltaTime;
         }
     }
 }
