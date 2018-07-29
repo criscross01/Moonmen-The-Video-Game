9dbfac302dc9307d3dddc908f432129321829726
ElliottBenoit <40448267+ElliottBenoit@users.noreply.github.com>
2018-07-29 17:39:53 -0500

    MENU
    YAY MENU

---
+++ Moonmen The Video Game/Assets/Scripts/ButtonFunctionality.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctionality : MonoBehaviour
+{
+
+    public void StartButton ()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OptionsButton ()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitButton ()
+    {
+        Application.Quit();
+    }
+}
